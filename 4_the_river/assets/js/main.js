32fc561e548d7dcce69cb040df4f14200b9a8172
Edward Colley <edward.colley@outlook.com>
2019-06-05 16:23:03 -0700

    repull

--- 4_the_river/assets/js/main.js
+++ 4_the_river/assets/js/main.js
@@ -1,9 +1,29 @@
+$(document).ready(initializeApp);
+
+function initializeApp(){
+var stoneResource = new Resource("stone");
+$('.stone').on('click', 'button', stoneResource.decrementResource);
+
+}
 class Resource{
+  
   constructor( name ){
     this.name = name;
-    this.value = null;
+    this.value = this.getResourceValue();
+    this.decrementResource = this.decrementResource.bind(this);
   }
-  getCurrentResourceValue(){
-    
+  
+  getResourceValue(){
+    var value = $('.resource.stone span').text();
+    var integerValue = parseInt(value);
+    return integerValue;
+  }
+
+  decrementResource(){
+    debugger;
+    var newValue = this.value - 1;
+    var updatedValue = $('.stone span').text(newValue);
+    return updatedValue;
   }
 }
+
